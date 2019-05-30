072013485f884f42ec625add3633526c2766f8d8
Grustentier <29798768+Grustentier@users.noreply.github.com>
2019-05-30 12:01:11 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -2,199 +2,77 @@
 
 <head><title></title>
 <meta charset="utf-8">
+<link rel="stylesheet" href="resources/css/style.css">
 <link rel="stylesheet" href="resources/css/jquery-ui/jquery-ui.css">
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <link rel="stylesheet" href="resources/css/dropzone.css">
+<link rel="stylesheet" href="resources/css/croppie/croppie.css">
 <script type="text/javascript" src="resources/js/jquery/jquery-3.3.1.js"></script>
 <script src="resources/js/jquery-ui/jquery-ui.js"></script>
 <script src="resources/js/dropzone/dropzone.js"></script>
 <script src="resources/js/chart/Charts.js"></script>
 <script src="resources/js/chart/Chart.bundle.js"></script>
 <script src="resources/js/chart/Chart.utils.js"></script>
-<script src="resources/js/colors.js"></script>
-
- 
-
-<style>
-body{margin:0px;padding:0px}
-.draggableLandmarks { position:inherit;float:right;padding: 1em;}
-.resetLandmarks { position:inherit;float:right;padding: 1em;}
-.droppable { width: 800px; height: 800px;  float: left; border:1px solid blue }
-.landmark { width:18px; height:18px; cursor: move;text-align: center;font-size:9px;-moz-border-radius: 15px;-khtml-border-radius: 15px;-webkit-border-radius: 15px;-o-border-radius: 15px;border-radius: 15px; }
-.landmark span{top: 3px;position: relative;}
-.myDropzone{min-height: 150px;overflow: auto;border: 2px solid rgba(0, 0, 0, 0.3);background: #b3e8da;padding: 20px 20px;}
-.myDropzone:hover{border:2px solid green;color:green;}
-.myButton{background-color: rgb(54, 162, 235);color: white;border-bottom-right-radius: 5px;border-bottom-left-radius: 5px;border-top-right-radius: 5px;border-top-left-radius: 5px;cursor: pointer;padding: 10px;border: none;}
-.myButtonSmall{background-color: rgb(54, 162, 235);color: white;border-bottom-right-radius: 5px;border-bottom-left-radius: 5px;border-top-right-radius: 5px;border-top-left-radius: 5px;cursor: pointer;padding: 5px;border: none;}
-#resultContainer{display: none;}
-.dz-message{color: rgba(0, 0, 0, 0.3);font-size: 30px;text-align: center;margin-top: 57px;}
-</style>
+<script src="resources/js/croppie/croppie.js"></script>
+<script src="resources/js/system.js"></script> 
+<script src="resources/js/colors.js"></script> 
   
 </head>
 
 <body>
-<button id="newMeasurement" class="ui-button myButton" style="top:10px;left:10px;">New Landmark Positioning</button> 
-<br><br><br>
-<div id="tabs"> <ul> </ul> </div>
+<div id="menu">
+<div id="MenuEntry1" onclick="$('html, body').stop().animate({'scrollTop': $('#descriptionContainer').offset().top-50}, 500, 'swing');">About</div>
+<div id="MenuEntry2" onclick="$('#measurementContainer').show('slow',function(){$('#resultContainer').show(); $('html, body').stop().animate({'scrollTop': $('#measurementContainer').offset().top-50}, 500, 'swing');});">Measure</div>
+<div id="MenuEntry3" onclick="$('html, body').stop().animate({'scrollTop': $('#resultContainer').offset().top-50}, 500, 'swing');">Results</div>
+</div>
+<div id="descriptionContainer"><div>
+<table>
+<tr><td style="width: 100px;background: url('resources/images/face.png') no-repeat center center transparent;"></td><td>
+Mithilfe dieser App K&ouml;nnen Sie prüfen, wie ähnlich zwei oder mehrere Gesichter zueinander sind. 
+Die Grundlage daf&uuml;r bilden sogenannte Gesichtslandmarken. &Uuml;ber diese können korrespondierende Punkte spezifischer Gesichtsmerkmale markiert werden. 
+Die Landmarken weisen dann untereinander unterschiedliche Distanzen zueinander auf. 
+Die Darstellung unterschiedlicher Distanzverhältnisse kann abschließend einen Überblick darüber verschaffen, inwiefern
+unterschiedliche Gesichter, geringe Distanzen und somit große Ähnlichkeiten in Ihren Gesichtsmerkmalen aufweisen.
+</td></tr>
+<tr><td style="width: 100px;background: url('resources/images/privacy.png') no-repeat center center transparent;"></td><td>
+Von viel größerer Bedeutung für den Anwender ist das Speichern der in dieser App verarbeiteten Daten.  
+An dieser Stelle wird versichert, dass alle Bilder die über eine DropZone o.Ä. für die App zur Verfügung gestellt werden, 
+lediglich für die Darstellung im Browser vorgesehen sind, um die Funktion der App zu ermöglichen. 
+Eine Speicherung der Bilder im Backend erfolgt somit nicht.
+Genau so verhält sich die App im Umgang mit personenspezifischen Positionen einzelner Landmarken.
+Sie als Nutzer können frei entscheiden, inwiefern Sie Landmarkenpositionen der Wissenschaft zut Verfügung stellen möchten.
+Dabei würden lediglich die Positionen der Landmarken gespeichert und somit keinerlei Informationen dar&uuml;ber, welche auf die Identit&auml;t einer
+Person zurückschließen lässt.
+</td></tr>
+</table>
 
+</div>
+<div style="text-align: center;margin-top: 60px;">
+<button id="startMeasurement" class="myBigButton" style=" background:MediumSeaGreen">Start</button>
+</div>
+</div>
+<div id="measurementContainer">
+	<button id="newMeasurement" class="myButton" style="margin-top: -47px;position: absolute;background:MediumSeaGreen">New Landmark Positioning</button>
+	<div id="tabs"> <ul> </ul> </div>
+</div>
+<div id="resultContainer">
  
+<!--img src="resources/images/arrow.png" style="width: 130px;padding: 20px;"/-->
+<br>
+<button id='process' class='myBigButton'>Update Results</button>
+ 
+<br><br>
+<div id="canvasContainer"><canvas id='canvas'></canvas></div>
+ 
+<button id='saveData' class='myBigButton' style="margin-top: 40px">Save Data</button>
+<div id="console"></div>
+</div>
 
 
 
 
 
 <script>
-var chartLabels = ["b1-b2/a6-a11","b1-b5/a6-a11","a12-a14/a15-a16","a12-a14/b1-a13","a12-a14/b1-b5","a15-a16/a6-a11","a15-a16/a7-a10","a8-a9/a15-a16","a8-a9/b1-a13","a8-a9/b1-b5","a7-a10/b1-b5","b1-a13/a7-a10","b1-a13/b1-b5","b1-a13/a6-a11","a12-a14/a18-a20","a15-a16/a18-a20","a6-a11/a18-a20","a8-a9/a18-a20","a7-a10/a18-a20"];
-var tabCount = 0;
-
-function createLandmarkMenu(tabCount){
-	jQuery("#draggableLandmarks-"+tabCount).empty();
-	as = ["a6","a7","a8","a9","a10","a11","a12","a13","a14","a15","a16","a18","a20"]
-	bs = ["b1","b2","b5"]
-	as.forEach(function(element) {
-		jQuery("#draggableLandmarks-"+tabCount).append("<div class='landmark' style='background:red;' id='"+element+"-"+tabCount+"'><span>"+element+"</span></div><br>");
-	}); 
-	bs.forEach(function(element) {
-		jQuery("#draggableLandmarks-"+tabCount).append("<div class='landmark' style='background:green;' id='"+element+"-"+tabCount+"'><span>"+element+"</span></div><br>");
-	});
-	jQuery("#draggableLandmarks-"+tabCount).find( ".landmark" ).draggable({ 
-		revert: "invalid",
-		stop: function( event, ui ) {
-			//console.log(ui);
-		}
-	});
-} 
-
-function createNewTab(){
-	tabCount = tabCount + 1;
-	var tabs = $( "#tabs" ).tabs();
-	var ul = tabs.find( "ul" );
-	$("#tabs").append("<div id='tabs-"+tabCount+"'></div>"); 
-	$( "<li style='background:"+colors[tabCount]+"'><a href='#tabs-"+tabCount+"'>Person "+tabCount+"</a></li>" ).appendTo( ul );
-
-	appendContentToTab(tabCount);
-	
-	tabs.tabs( "refresh" );  
-
-	$("#tabs ul li a[href='#tabs-"+tabCount+"']").trigger("click"); 
-	$("#resultContainer").show("slow",function(){});
-}
-
-function appendContentToTab(tabCount){
-	//$("#tabs-"+tabCount+"").append("<div id='draggableLandmarks-"+tabCount+"' class='ui-widget-content draggableLandmarks'> </div>");
-	//createLandmarkMenu(tabCount);
-
-	//$("#tabs-"+tabCount+"").append("<button id='resetLandmarks-"+tabCount+"' class='ui-button resetLandmarks'>Reset</button>")
-	
-
-	$("#tabs-"+tabCount+"").append("<table><tr><td><img src='resources/images/template.png'/></td><td><div id='droppable-"+tabCount+"' class='ui-widget-header droppable'> </div></td><td><div id='draggableLandmarks-"+tabCount+"' class='ui-widget-content draggableLandmarks'> </div><br><button id='resetLandmarks-"+tabCount+"' class='resetLandmarks myButtonSmall'>Reset</button></td></tr><tr><td></td><td><div id='dropzone-"+tabCount+"' class='myDropzone' ><div class='dz-message needsclick'>Drop your file with a frontal face here or click to upload.</div></div><div id='previewContainer-"+tabCount+"'> </div></td><td></td></tr></table>");
-	initDropable4Tab(tabCount);
-	initDropzone4Tab(tabCount);
-	createLandmarkMenu(tabCount);
-	$("#resetLandmarks-"+tabCount).click(function(){ 
-		createLandmarkMenu(tabCount);
-		//jQuery( "#droppable-"+tabCount).empty();
-	});
-
-	
-}
-
-function initDropable4Tab(tabCount){
-	jQuery( "#droppable-"+tabCount).droppable({
-    	classes: {
-        	"ui-droppable-active": "ui-state-active",
-            "ui-droppable-hover": "ui-state-hover"
-        },
-	    drop: function( event, ui ) {
-		    $( this ).addClass( "ui-state-highlight" );
-
-		     /*var draggable = ui.draggable;
-		     var dragged = draggable.clone();      
-		     dragged.appendTo("#droppable-"+tabCount);
-		     dragged.resizable({containment: '#droppable-'+tabCount})
-		     */
-		} 
-    }); 
-}
-
-function initDropzone4Tab(tabCount){
-	jQuery("#dropzone-"+tabCount).dropzone({ 		 
-		url: "/",   
-		createImageThumbnails: true,
-		acceptedFiles: "image/*",
- 		addRemoveLinks: true,
- 		previewContainer: "#previewContainer-"+tabCount, 
-		 
-		success: function(file, response){
-			jQuery("#droppable-"+tabCount).empty(); 
-			jQuery("#droppable-"+tabCount).append("<img src='"+file.dataURL+"' style='width: 800px;'/>");
-			this.removeFile(file);  
-		},
-
-		queuecomplete: function(file, response){ },
-		
-		error: function(file, response){
-			var message = "error: " + response + " ("+file.name+")"; 
-			this.removeFile(file); 
-		} 
-	});
-}
-
-function euklidDistance(positionA, positionB){
-	return Math.sqrt( Math.pow((positionA.left-positionB.left), 2) + Math.pow((positionA.top-positionB.top), 2) );
-}
-
-function scrollTo(element){
-	$('html, body').animate({ scrollTop: ($(element).offset().top)}, 'slow');
-};
-
-function writeToFile(sText){
-	var fso = new ActiveXObject("Scripting.FileSystemObject");
-	var s = fso.CreateTextFile("test.txt", true);
-	s.WriteLine(sText);
-	s.Close();
-} 
-
-
-
-
-var config = {
-		type: 'line',
-		data: {
-			labels: chartLabels,			
-		},
-		options: {
-			responsive: true,
-			title: {
-				display: true,
-				text: 'Landmark Result'
-			},
-			tooltips: {
-				mode: 'index',
-				intersect: false,
-			},
-			hover: {
-				mode: 'nearest',
-				intersect: true
-			},
-			scales: {
-				xAxes: [{
-					display: true,
-					scaleLabel: {
-						display: true,
-						labelString: 'Month'
-					}
-				}],
-				yAxes: [{
-					display: true,
-					scaleLabel: {
-						display: true,
-						labelString: 'Value'
-					}
-				}]
-			}
-		}
-	};
 
 
  
@@ -202,18 +80,32 @@ var config = {
 $( document ).ready(function() {
 	 
 	$("#tabs").tabs();
+	createNewTab();
+	$( document ).tooltip();
 	
 	var ctx = document.getElementById('canvas').getContext('2d');	
 	window.myLine = new Chart(ctx, config);
+
+	$("#startMeasurement").click(function(){
+			$(this).hide("slow"); 
+			$('#measurementContainer').show('slow',function(){
+				$('#MenuEntry2').show("slow"); 
+				$('#MenuEntry3').show("slow"); 
+				$('#resultContainer').show(); 
+				$('html, body').stop().animate({
+					'scrollTop': $('#measurementContainer').offset().top-50}, 500, 'swing');
+				
+			});
+	});
 	
 	$("#newMeasurement").click(function(){ createNewTab();});
 
 	$( "#tabs" ).tabs({ active: 0 });
 	$("#process").click(function(){   
-	
+		RESULT_JSON = [];
 		while (window.myLine.data.datasets.length && window.myLine.data.datasets.length > 0) { window.myLine.data.datasets.pop();}		 
 		
-		tabCount = $('#tabs >ul >li').length;
+		var tabCount = $('#tabs >ul >li').length;		
 		for(i = 1; i < tabCount + 1; i++){
 			 
 			$("#tabs ul li a[href='#tabs-"+i+"']").trigger("click"); 
@@ -256,7 +148,32 @@ $( document ).ready(function() {
 				  fill: false,
 				  borderColor: colors[i],
 				  data: [b1_b2_a6_a11,b1_b5_a6_a11,a12_a14_a15_a16,a12_a14_b1_a13,a12_a14_b1_b5,a15_a16_a6_a11,a15_a16_a7_a10,a8_a9_a15_a16,a8_a9_b1_a13,a8_a9_b1_b5,a7_a10_b1_b5,b1_a13_a7_a10,b1_a13_b1_b5,b1_a13_a6_a11,a12_a14_a18_a20,a15_a16_a18_a20,a6_a11_a18_a20,a8_a9_a18_a20,a7_a10_a18_a20]
-				});  
+			}); 
+
+		 
+			var personJSON = {
+								"landmarks":{
+									"a6":[a6.offset().left,a6.offset().top],
+									"a7":[a7.offset().left,a7.offset().top],
+									"a8":[a8.offset().left,a8.offset().top],
+									"a9":[a9.offset().left,a9.offset().top],
+									"a10":[a10.offset().left,a10.offset().top],
+									"a11":[a11.offset().left,a11.offset().top],
+									"a12":[a12.offset().left,a12.offset().top],
+									"a13":[a13.offset().left,a13.offset().top],
+									"a14":[a14.offset().left,a14.offset().top],
+									"a15":[a15.offset().left,a15.offset().top],
+									"a16":[a16.offset().left,a16.offset().top],
+									"a18":[a18.offset().left,a18.offset().top],
+									"a20":[a20.offset().left,a20.offset().top],
+									"b1":[b1.offset().left,b1.offset().top],
+									"b2":[b2.offset().left,b2.offset().top],
+									"b5":[b5.offset().left,b5.offset().top]
+								} 
+			} 
+			
+			RESULT_JSON.push(personJSON);
+			
 		}
 		
 		window.myLine.update(); 
@@ -267,12 +184,9 @@ $( document ).ready(function() {
 
 	
 	$("#saveData").click(function(){
-		alert("NOT IMPLEMENTED!");   
-		/*
-    	var json = jQuery.parseJSON( '{ "name": "John" }' );
-    	var data = JSON.stringify(json);
-    	var stream = IO.newOutputStream("test.txt", "text nocreate append");
-    	*/
+		console.log(JSON.stringify(RESULT_JSON)); 
+		$("#console").text(JSON.stringify(RESULT_JSON));
+		$('html, body').stop().animate({'scrollTop': $('#console').offset().top-50}, 500, 'swing');
 	});
 	
 	 
@@ -281,19 +195,7 @@ $( document ).ready(function() {
 
 </script>
 
-<div id="resultContainer">
 
-<center>
-<img src="resources/images/arrow.png" style="width: 130px;padding: 20px;"/>
-<br>
-<button id='process' class='ui-button myButton'>Analyze</button>
-</center>
-<br><br>
-<div id="canvasContainer" style="width:100%;"><canvas id='canvas'></canvas></div>
-<center>
-<button id='saveData' class='ui-button myButton'>Save Data</button>
-</center>
-</div>
 
 </body>
 
